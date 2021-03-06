29304cf5752d3ea1fd73ae4f76f1da4c00950e83
NikolaSedProgger <forniteturbokosh@gmail.com>
2021-03-06 13:42:47 +0300

    Изменил main.py ; README.md ; template.html
    - Удалил лишние файлы из репозитория
    - Разрешил пользователю указать другой путь к файлу
    - Добавил argparse в код
    - Добавил вычисление надписи год/лет/года
    - Предоставил образец входных данных

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-import datetime
+from datetime import datetime
 from http.server import HTTPServer, SimpleHTTPRequestHandler
 import pandas
 from collections import defaultdict
-
 from jinja2 import Environment, FileSystemLoader, select_autoescape
+import argparse
 
 env = Environment(
     loader=FileSystemLoader('.'),
@@ -12,20 +12,39 @@ env = Environment(
 
 template = env.get_template('template.html')
 
-current_date = f'{datetime.date.today()}'
 year_started = 1920
+years = datetime.today().year-year_started
+last_year_num = int(list(str(years))[-1])
 
-wines = pandas.read_excel('wine3.xlsx', sheet_name='Лист1', na_values='nan', keep_default_na=False).to_dict(orient='records')
+if last_year_num == 1:
+    a = "год"
+elif last_year_num >= 2 and last_year_num < 5:
+    a = "года"
+elif last_year_num >= 5 and last_year_num < 10 or last_year_num == 0:
+    a = "лет"
+
+parser = argparse.ArgumentParser(
+    description='Чтобы запустить сайт (index.html) вам потребуется указать название Exel файла с данными для вин'
+)
+parser.add_argument('file', help='Название файла')
+args = parser.parse_args().file
+
+try:
+    wines = pandas.read_excel(args, sheet_name='Лист1', na_values='nan', keep_default_na=False).to_dict(orient='records')
+except:
+    print("Файла, который вы указали, не существует. Использован wines.xlsx")
+    wines = pandas.read_excel("wines.xlsx", sheet_name='Лист1', na_values='nan', keep_default_na=False).to_dict(orient='records')
+
+
 wine_assortment = defaultdict(list)
 for bottle in wines:
     wine_assortment[bottle['Категория']].append(bottle)
-    
 
 rendered_page = template.render(
-    years=int(current_date[:4])-year_started,
+    a = a,
+    years=years,
     wine_assortment=wine_assortment
 )
-
 
 with open('index.html', 'w', encoding="utf8") as file:
     file.write(rendered_page)
